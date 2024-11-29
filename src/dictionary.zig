833e669cf6bef46b37866aa3a7248d42d263a891
defval <m.bovtunov@efg.gg>
2024-11-29 03:00:40 +0300

    test: parseWords happy path

--- src/dictionary.zig
+++ src/dictionary.zig
@@ -1,8 +1,11 @@
-const std = @import("std");
 const fs = @import("std").fs;
 const heap = @import("std").heap;
 const debug = @import("std").debug;
 const mem = @import("std").mem;
+const testing = @import("std").testing;
+
+const Allocator = mem.Allocator;
+const ArrayList = @import("std").ArrayList;
 
 const MB: usize = 1024 * 1024;
 const DEBUG_LOADED =
@@ -11,11 +14,12 @@ const DEBUG_LOADED =
 
 // Dictionary contains words used by clikey.
 pub const Dictionary = struct {
+    alloc: Allocator,
     path: []const u8,
-    words: std.ArrayList([]const u8),
+    words: ArrayList([]const u8),
 
     // init inits word Dictionary from file by path using alloc Allocator.
-    pub fn init(alloc: mem.Allocator, path: []const u8) !*const Dictionary {
+    pub fn init(alloc: Allocator, path: []const u8) !*const Dictionary {
         // open dictionary file
         const f = try fs.cwd().openFile(path, .{});
         defer f.close();
@@ -24,6 +28,7 @@ pub const Dictionary = struct {
         const words = try parseWords(alloc, data);
         const result = alloc.create(Dictionary) catch return error.OutOfMemory;
         result.* = Dictionary{
+            .alloc = alloc,
             .path = path,
             .words = words,
         };
@@ -33,6 +38,7 @@ pub const Dictionary = struct {
     // deinit deinits Dictionary memory.
     pub fn deinit(self: *const Dictionary) void {
         self.words.deinit();
+        self.alloc.destroy(self);
     }
 
     pub fn printWords(self: *const Dictionary) void {
@@ -45,9 +51,9 @@ pub const Dictionary = struct {
 };
 
 // parse parses data into std.ArrayList of []const u8 using alloc allocator.
-fn parseWords(alloc: mem.Allocator, data: []u8) !std.ArrayList([]const u8) {
+fn parseWords(alloc: Allocator, data: []const u8) !ArrayList([]const u8) {
     // array of words
-    var words = std.ArrayList([]const u8).init(alloc);
+    var words = ArrayList([]const u8).init(alloc);
     // split file by line
     var lines = mem.splitSequence(u8, data, "\n");
     while (lines.next()) |line| {
@@ -60,4 +66,25 @@ fn parseWords(alloc: mem.Allocator, data: []u8) !std.ArrayList([]const u8) {
         }
     }
     return words;
+}
+
+test "parseWords: parses sequence of words into array list" {
+    const a = testing.allocator;
+    var list = ArrayList([]const u8).init(a);
+    defer list.deinit();
+
+    try list.append("test");
+    try list.append("mest");
+    try list.append("vest");
+
+    const input: []const u8 = "test mest vest";
+
+    var words = try parseWords(a, input);
+    defer words.deinit();
+
+    while (list.items.len != 0) {
+        const expected: []const u8 = list.pop();
+        const actual: []const u8 = words.pop();
+        try testing.expect(mem.eql(u8, expected, actual));
+    }
 }
